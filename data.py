a70938c5fb89cf574851bf3e25bdea86903c7b3f
Muhammad Shamshad Aslam <75529769+Aizad-eng@users.noreply.github.com>
2023-09-26 20:22:48 +0500

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import requests
 def collect_quiz_questions():
-    response = requests.get(url="https://opentdb.com/api.php?amount=5&type=boolean")
+    response = requests.get(url="https://opentdb.com/api.php?amount=50&type=boolean")
     quiz_questions = response.json()["results"]
     print(quiz_questions)
     return quiz_questions
